e18da624cec4ebdbc06f227db608ede4ac119c93
Andro Babu <rambabusaravanan@gmail.com>
2022-07-17 11:24:42 +0800

    Update bind-call-apply.js

--- js/bind-call-apply.js
+++ js/bind-call-apply.js
@@ -3,7 +3,7 @@ function fn(a, b, c) {
   return 'hello world';
 }
 
-/** bind **/
+/** bind : bind & return fn : .bind(this, arg1, arg2)(arg3) **/
 f = fn.bind({ z: 'z' });
 x = f();          // z undefined undefined undefined // x -> 'hello world'
 x = f(1, 2);      // z 1 2 undefined // x -> 'hello world'
@@ -11,10 +11,10 @@ f = fn.bind({ z: 'z' }, 1);
 x = f();          // z 1 undefined undefined  // 'hello world'
 x = f(2);         // z 1 2 undefined // 'hello world'
 
-/** call **/
+/** call : bind & exec fn : .call(this, arg1, arg2, arg3) **/
 x = fn.call({ z: 'z' });        // z undefined undefined undefined  // x -> 'hello world'
 x = fn.call({ z: 'z' }, 1, 2);  // z 1 2 undefined // x -> 'hello world'
 
-/** apply **/
+/** apply : bind & exec fn : .apply(this, [arg1, arg2, arg3])  **/
 x = fn.apply({ z: 'z' });           // z undefined undefined undefined  // x -> 'hello world'
 x = fn.apply({ z: 'z' }, [1, 2]);   // z 1 2 undefined // x -> 'hello world'
